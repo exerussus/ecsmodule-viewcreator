c63662af56c778b1018409c5409c4ed32f6303f6
exerussus <solovyov.production@gmail.com>
2024-10-12 22:36:15 +0300

    Auto updated to version 1.0.2

--- ViewCreatorGroup.cs
+++ ViewCreatorGroup.cs
@@ -1,6 +1,5 @@
 ﻿using Exerussus._1EasyEcs.Scripts.Core;
 using Exerussus._1EasyEcs.Scripts.Custom;
-using Exerussus.EasyEcsModules.ViewCreator.System;
 using Leopotam.EcsLite;
 
 namespace Exerussus.EasyEcsModules.ViewCreator
@@ -8,11 +7,6 @@ namespace Exerussus.EasyEcsModules.ViewCreator
     public class ViewCreatorGroup : EcsGroup<ViewCreatorPooler>
     {
         public ViewCreatorSettings Settings = new();
-        
-        protected override void SetInitSystems(IEcsSystems initSystems)
-        {
-            initSystems.Add(new ViewCreatorSystem());
-        }
 
         protected override void SetSharingData(EcsWorld world, GameShare gameShare)
         {
--- ViewCreatorPooler.cs
+++ ViewCreatorPooler.cs
@@ -1,18 +1,73 @@
-﻿using Exerussus._1EasyEcs.Scripts.Core;
+﻿using System;
+using Exerussus._1EasyEcs.Scripts.Core;
 using Exerussus._1EasyEcs.Scripts.Custom;
+using Exerussus._1OrganizerUI.Scripts.Pooling;
+using Exerussus.EasyEcsModules.ViewCreator.MonoBehaviours;
 using Leopotam.EcsLite;
+using UnityEngine;
 
 namespace Exerussus.EasyEcsModules.ViewCreator
 {
     public class ViewCreatorPooler : IGroupPooler
     {
-        public void Initialize(EcsWorld world)
+        public void BeforeInitialize(EcsWorld world, GameShare gameShare, GameContext gameContext, string groupName)
         {
-            AssetLoadingMark = new PoolerModule<ViewCreatorData.AssetLoadingMark>(world);
-            AssetViewApi = new PoolerModule<ViewCreatorData.AssetViewApi>(world);
+            Settings = gameShare.GetSharedObject<ViewCreatorSettings>();
         }
 
+        public void Initialize(EcsWorld world)
+        {
+            _world = world;
+            AssetLoadingMark = new PoolerModule<ViewCreatorData.AssetLoadingMark>(world);
+            AssetViewApi = new PoolerModule<ViewCreatorData.AssetViewApi>(world);
+            
+            if (Settings.AddressableInfos == null || Settings.AddressableInfos.Length == 0) return;
+
+            foreach (var addressableInfo in Settings.AddressableInfos) _assetPooler.Initialize(addressableInfo.name, addressableInfo.path);
+        }
+
+        private EcsWorld _world;
+        private readonly AssetPool<AssetViewApi> _assetPooler = new();
+        public  ViewCreatorSettings Settings { get; private set; }
         public PoolerModule<ViewCreatorData.AssetLoadingMark> AssetLoadingMark { get; private set; }
         public PoolerModule<ViewCreatorData.AssetViewApi> AssetViewApi { get; private set; }
+
+        public void CreateView(int entity, string assetName, string addressablePath, Vector3 position, Action<AssetViewApi> execute)
+        {
+            AssetLoadingMark.Add(entity);
+            var packedEntity = _world.PackEntity(entity);
+            
+            _assetPooler.GetAndExecute(assetName, addressablePath, position, api =>
+            {
+                if (packedEntity.Unpack(_world, out var e))
+                {
+                    ref var newApi = ref AssetViewApi.Add(e);
+                    newApi.Value = api;
+                    AssetLoadingMark.Del(e);
+                    newApi.Value.Release = () => _assetPooler.Release(api);
+                    execute(api);
+                }
+                else _assetPooler.Release(api);
+            });
+        }
+        
+        public void CreateEntityView(string assetName, string addressablePath, Vector3 position, Action<AssetViewApi, int> execute)
+        {
+            var newEntity = _world.NewEntity();
+            var packedEntity = _world.PackEntity(newEntity);
+            AssetLoadingMark.Add(newEntity);
+            _assetPooler.GetAndExecute(assetName, addressablePath, position, api =>
+            {
+                if (packedEntity.Unpack(_world, out var entity))
+                {
+                    ref var newApi = ref AssetViewApi.Add(entity);
+                    newApi.Value = api;
+                    AssetLoadingMark.Del(entity);
+                    newApi.Value.Release = () => _assetPooler.Release(api);
+                    execute(api, newEntity);
+                }
+                else _assetPooler.Release(api);
+            });
+        }
     }
 }
